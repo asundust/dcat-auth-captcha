a24ec135823773da60d9225384f093ed2681bbbf
塵世不再 <asundust@qq.com>
2021-05-07 17:11:37 +0800

    Update version.php

--- version.php
+++ version.php
@@ -5,4 +5,7 @@ return [
         '移植[https://github.com/asundust/auth-captcha](https://github.com/asundust/auth-captcha)第一版',
         '大版本号跟随dcat-admin版本号走',
     ],
+    '2.0.1' => [
+        '兼容guzzlehttp/guzzle 7.*版本',
+    ],
 ];
