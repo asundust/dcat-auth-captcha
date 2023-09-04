04842b2d659afc7771780cd1e41c8c7ca3448176
asundust <asundust@qq.com>
2023-09-04 22:07:56 +0800

    优化错误代码

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('dcat-auth-captcha', Controllers\DcatAuthCaptchaController::class . '@index');
 
 if (config('admin.auth.enable', true)) {
-    $authController = config('admin.extensions.dcat-auth-captcha.controller', Controllers\DcatAuthCaptchaController::class);
-    Route::get('auth/login', $authController . '@getLogin');
-    Route::post('auth/login', $authController . '@postLogin');
+    Route::get('auth/login', Controllers\DcatAuthCaptchaController::class . '@getLogin');
+    Route::post('auth/login', Controllers\DcatAuthCaptchaController::class . '@postLogin');
 }
